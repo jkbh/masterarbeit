222b7945e3a80e735f29bfc658154c896cc1c6ac
Jakob Horbank <jakob_h@outlook.de>
2023-12-12 12:31:00 +0100

    code: update data download script (wip)

--- experiments/data.sh
+++ experiments/data.sh
@@ -1,6 +1,19 @@
-file="msnotesannotator_audition_certificates_data-master(1)(2).zip"
-
-mkdir data
+if [[ ! -d "data" ]]; then
+	mkdir data
+fi
 cd data
-wget "https://www.fdr.uni-hamburg.de/record/13525/files/$file"
-unzip $file
+
+# echo "audition certificates"
+# file="msnotesannotator_audition_certificates_data-master(1)(2).zip"
+# wget "https://www.fdr.uni-hamburg.de/record/13525/files/$file"
+# unzip $file
+
+echo "manuscript cultures"
+if [[ ! -d "manuscript_cultures" ]]; then
+	mkdir manuscript_cultures  
+fi
+cd manuscript_cultures
+for ((i = 1; i <= 19; i++)); do
+	echo "manuscript cultures $i"
+	wget "https://www.csmc.uni-hamburg.de/publications/mc/mc$i/mc$i-full.pdf"
+done
